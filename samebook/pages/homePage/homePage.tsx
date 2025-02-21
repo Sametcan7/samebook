aeea8087280a3b52426f44e7036e05703367bc73
Samet Can Akcaalan <sametcan.akcaalan7@gmail.com>
2025-02-22 02:30:58 +0300

    delete

--- samebook/pages/homePage/homePage.tsx
+++ samebook/pages/homePage/homePage.tsx
@@ -2,10 +2,15 @@ import Main from "./main";
 import LeftSide from "./leftSide";
 import RightSide from "./rightSide";
 import PostModalContextProvider from "@/samebook/contexts/postModalTrigger";
+import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <div className="mx-auto h-full  3xl:max-w-[1440px] pt-[var(--header-height)]">
+    <div className="mx-auto h-full pt-[var(--header-height)] 3xl:max-w-[1440px]">
+      <Link className="bg-white" href="/signup">
+        sign
+      </Link>
+
       <div className="flex pt-4">
         {/* Left */}
         <LeftSide />
